326d1fd3e683ad5739b90fd6c37c76205b6895a7
alle0017 <allegretti0017@gmail.com>
2025-07-18 10:08:08 +0200

    continuing branch as performance in list rendering is interesting

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,6 @@
-//import { html } from "../core/Parser/node/index.js"
-import {$signal,html} from "../core/index.js"
+import { html as v2 } from "../core/Parser/node/index.js"
+import {$signal, html as v1} from "../core/index.js"
 /**@import {VNode} from "../core/Parser/node/VNode"*/
-
 /**
  * 
  * @param {VNode<HTMLElement>[]} tree 
@@ -9,33 +8,52 @@ import {$signal,html} from "../core/index.js"
 function render(tree) {
       document.body.append(...(tree).flatMap(v => v.render()))
 }
-console.time('simple node rendering')
-render(html`
-      <div style="display: flex; justify-content: center; flex-direction: column;">
-            <h1>hello world</h1>
-            <p style="color: #aaa">my simple renderer</p>
-            <div>testing new renderer that uses new functional parser</div>
-      </div>
-`)
-console.timeEnd('simple node rendering')
+setTimeout(() => {
+      tests(v2);
 
-console.time('simple node rendering with interpolation')
-render(html`
-      <h1>hello world ${'hi'}</h1>
-`)
-console.timeEnd('simple node rendering with interpolation')
+      tests(v1);
 
+}, 1000);
 
-console.time('rendering of a list of values')
-render(html`
-      - List
-      <ul>${new Array(1000).fill(1).map((_,i) => html`<li style=${i%2? 'color: red;': 'color: blue;'}>${i}</li>`)}</ul>
-`)
-console.timeEnd('rendering of a list of values')
-
-console.time('counter implementation')
-const test_counter = $signal(0)
-render(html`
-      <button @click=${() => test_counter.value++}>${test_counter}</button>
-`)
-console.timeEnd('counter implementation')
+function test_1(html) {
+      console.log("TEST 1\n")
+      console.time('simple node rendering')
+      render(html`
+            <div style="display: flex; justify-content: center; flex-direction: column;">
+                  <h1>hello world</h1>
+                  <p style="color: #aaa">my simple renderer</p>
+                  <div>testing new renderer that uses new functional parser</div>
+            </div>
+      `)
+      console.timeEnd('simple node rendering')
+}
+function test_2(html) {
+      console.log("\nTEST 2\n")
+      console.time('simple node rendering with interpolation')
+      render(html`
+            <h1>hello world ${'hi'}</h1>
+      `)
+      console.timeEnd('simple node rendering with interpolation')
+}
+function test_3(html) {
+      console.log("\nTEST 3\n")
+      console.time('rendering of a list of values')
+      render(html`
+            - List
+            <ul>${new Array(1000).fill(1).map((_,i) => html`<li style=${i%2? 'color: red;': 'color: blue;'}>${i}</li>`)}</ul>
+      `)
+      console.timeEnd('rendering of a list of values')
+}
+function test_4(html) {
+      console.log("\nTEST 4\n")
+      console.time('counter implementation')
+      const test_counter = $signal(0)
+      render(html`
+            <button @click=${() => test_counter.value++}>${test_counter}</button>
+      `)
+      console.timeEnd('counter implementation')
+}
+function tests(html) {
+      console.log("TESTING IMPLEMENTATION OF HTML FUNCTION\n\n\n\n\n")
+      test_3(html)
+}
